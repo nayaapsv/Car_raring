d77f8010942f379a9b980de1111065ec5b4c95a6
nayaapsv <66906453+nayaapsv@users.noreply.github.com>
2020-08-14 18:10:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var canvas ,bgImg;
+var gameState=0;
+var playerCount;
+var database ,form ,player ,game;
+var allPlayers;
+var car1,car2,car3,car4;
+var cars=[];
+
+function setup(){
+    createCanvas(displayWidth-20,displayHeight-30);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+    
+    
+}
+
+function draw(){
+    background("white");
+   if (playerCount==4){
+       game.update(1);
+   }
+    if (gameState==1){
+      clear();  
+      game.play();
+    }
+    
+    //drawSprites();
+}
+
